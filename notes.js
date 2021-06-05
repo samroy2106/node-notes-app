3559f920946e044c2897967bd41e7eb35fee24d8
Samarth Roy <samarthroy2106@gmail.com>
2021-06-05 21:34:27 +0530

    addNote, removeNote functionality implemented

---
+++ notes.js
@@ -0,0 +1,60 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+const getNotes = () => {
+  return 'Your notes...'
+}
+
+const addNote = (title, body) => {
+  const notes = loadNotes()
+  const duplicateNotes = notes.filter(note => {
+    return note.title === title
+  })
+
+  if(duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    })
+
+    saveNotes(notes)
+    console.log(chalk.green('New note added!'))
+  } else {
+    console.log(chalk.red('Note title taken!'))
+  }
+}
+
+const removeNote = (title) => {
+  const notes = loadNotes()
+  const remainingNotes = notes.filter(note => {
+    return note.title != title
+  })
+
+  if(notes.length - remainingNotes.length === 1) {
+    saveNotes(remainingNotes)
+    console.log(chalk.green('Note removed!'))
+  } else {
+    console.log(chalk.red('No note with given title found!'))
+  }
+}
+
+const saveNotes = (notes) => {
+  dataJSON = JSON.stringify(notes)
+  fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+  try{
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
+  } catch(e) {
+    return []
+  }
+}
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+  removeNote: removeNote
+}
